551447ebe3c08d03ad08c1f676b433f773965394
swapnalinikumbh <nikumbh05@gmail.com>
2023-10-25 11:42:43 +0530

    E-commerce-store

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Navbar from "./component/Navbar";
+import { Switch, Route } from "react-router-dom";
+import Products from "./component/Products";
+import Product from "./component/Product";
+import Cart from "./component/Cart";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Products} />
+        <Route exact path="/products/:id" component={Product} />
+        <Route exact path="/cart" component={Cart} />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
